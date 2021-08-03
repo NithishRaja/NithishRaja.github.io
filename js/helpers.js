ca91fbb675e37b43251683e533ede7f87cd60132
nithish <nithish98raja@gmail.com>
2021-08-03 21:24:37 +0530

    move code to create card into helpers

--- js/helpers.js
+++ js/helpers.js
@@ -7,3 +7,25 @@
 function getTileId(number, type){
   return "tile_"+number+"_"+type;
 }
+
+// Function to create a card element
+function createCardElement(id, image_url){
+  // Create a card
+  let card = document.createElement("div");
+  // Add id to card
+  card.setAttribute("id", id);
+  // Add class to card
+  card.classList.add("card");
+  card.classList.add("mb-3");
+  card.classList.add("shadow");
+  // Create image element
+  let image = document.createElement("img");
+  // Set image source
+  image.setAttribute("src", image_url);
+  // Add class to image
+  image.classList.add("card-img-top");
+  // Append image to card
+  card.appendChild(image);
+  // Return card
+  return card;
+}
--- js/initialise.js
+++ js/initialise.js
@@ -3,56 +3,46 @@
   *
   */
 
-// Get grid
-let grid = document.getElementById("grid");
+// Script to add tiles to grid
+(function(){
+  // Get grid
+  let grid = document.getElementById("grid");
 
-// Check if grid is Initialised
-if(grid){
-  // Iterate grid size and add rows
-  for(let i=0;i<grid_size;++i){
-    // Create row element
-    let row = document.createElement("div");
-    // Add id to row element
-    row.setAttribute("id", "row_"+i);
-    // Add class to row
-    row.classList.add("row");
-    // Iterate over cells in current row
-    for(let j=0;j<grid_size;++j){
-      // Create column element
-      let col = document.createElement("div");
-      // Add id to col element
-      col.setAttribute("id", "row_"+i+"_col_"+j);
-      // Add class to col
-      col.classList.add("col");
+  // Check if grid is Initialised
+  if(grid){
+    // Iterate grid size and add rows
+    for(let i=0;i<grid_size;++i){
+      // Create row element
+      let row = document.createElement("div");
+      // Add id to row element
+      row.setAttribute("id", "row_"+i);
+      // Add class to row
+      row.classList.add("row");
+      // Iterate over cells in current row
+      for(let j=0;j<grid_size;++j){
+        // Create column element
+        let col = document.createElement("div");
+        // Add id to col element
+        col.setAttribute("id", "row_"+i+"_col_"+j);
+        // Add class to col
+        col.classList.add("col");
+        // Initialise variable to hold id of tile
+        let id = i*grid_size+j;
 
-      // Create a card
-      let card = document.createElement("div");
-      // Initialise variable to hold id of card
-      let id = i*grid_size+j;
-      // Add id to card
-      card.setAttribute("id", getTileId(id, "front"));
-      // Set pair id
-      card.setAttribute("pair_id", pairId[id]);
-      // Add class to card
-      card.classList.add("card");
-      card.classList.add("mb-3");
-      card.classList.add("shadow");
-      card.classList.add("visible");
-      // Create image element
-      let image = document.createElement("img");
-      // Set image source
-      image.setAttribute("src", pair_image_map["default"]);
-      // Add class to image
-      image.classList.add("card-img-top");
-      // Append image to card
-      card.appendChild(image);
-      // Append card to column
-      col.appendChild(card);
+        // Create a card
+        let card = createCardElement(getTileId(id, "front"), pair_image_map["default"]);
+        // Set pair id
+        card.setAttribute("pair_id", pairId[id]);
+        // Make card visible
+        card.classList.add("visible");
+        // Append card to column
+        col.appendChild(card);
 
-      // Append column to row
-      row.appendChild(col);
+        // Append column to row
+        row.appendChild(col);
+      }
+      // Append row to grid
+      grid.appendChild(row);
     }
-    // Append row to grid
-    grid.appendChild(row);
   }
-}
+})();
