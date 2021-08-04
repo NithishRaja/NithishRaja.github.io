fb8604743ea51158aa63f45c71f7a1b32f9da5b3
nithish <nithish98raja@gmail.com>
2021-08-04 07:38:21 +0530

    remove excess comments

--- index.js
+++ index.js
@@ -16,10 +16,14 @@ function updateClickedTiles(id){
   }
   // Case 2: first tile clicked previously, update second tile
   else if(tile_clicked_first !== undefined && tile_clicked_second === undefined){
-    // Update clicked tile
-    tile_clicked_second = id;
-    // update status
-    status = true;
+    // Case 2a: second tile is different from first tile
+    if(tile_clicked_first !== id){
+      // Update clicked tile
+      tile_clicked_second = id;
+      // update status
+      status = true;
+    }
+    // Case 2b: first tile is clicked a second tile
   }
   // Case 3: Both tiles clicked previously, do nothing
   // Return status
@@ -106,34 +110,3 @@ function comparePairId(){
     });
   }
 })();
-
-// // Function to make the back tile visible and front tile invisible
-// function makeBackVisible(id){
-//   // Make front tile invisible and back tile visible
-//   document.getElementById(getTileId(id, "front")).classList.replace("visible", "invisible");
-//   document.getElementById(getTileId(id, "back")).classList.replace("invisible", "visible");
-// }
-//
-// // Function to make the front tile visible and back tile invisible
-// function makeFrontVisible(id){
-//   // Make back tile invisible and front tile visible
-//   document.getElementById(getTileId(id, "back")).classList.replace("visible", "invisible");
-//   document.getElementById(getTileId(id, "front")).classList.replace("invisible", "visible");
-// }
-//
-// (function(){
-//   // Iterate over all front tiles
-//   for(let i=1;i<=9;++i){
-//     // Get front tile
-//     frontTile = document.getElementById(getTileId(i, "front"));
-//     // Get back tile
-//     backTile = document.getElementById(getTileId(i, "back"));
-//     // Add click event listener to front tile
-//     if(frontTile){
-//       frontTile.addEventListener("click", function(event){
-//         updateClickedTiles(i);
-//         updateView(i);
-//       }, false);
-//     }
-//   }
-// })();
