cdafebdf358bdc7c1f3f5cc47084b3d9c832d917
nithish <nithish98raja@gmail.com>
2021-09-21 18:14:43 +0530

    use sweet alert to display popup

--- js/displayPopup.js
+++ js/displayPopup.js
@@ -3,27 +3,6 @@
   *
   */
 
-// Function to add click event listener to popup
-function initialisePopup(){
-  // Get popup element
-  let popup = document.getElementById("popup_background");
-  if(popup){
-    // Add click event listener
-    popup.addEventListener("click", function(){
-      popup.style.display = "none";
-    });
-  }
-}
-
 function displayPopup(){
-  // Get popup element
-  let popup = document.getElementById("popup_background");
-  if(popup){
-    // Get popup content element
-    content = document.getElementById("popup_content");
-    // Add text to popup content element
-    content.innerHTML = getTitbit();
-    // Make popup visible
-    popup.style.display = "block";
-  }
+  swal(getTitbit());
 }
